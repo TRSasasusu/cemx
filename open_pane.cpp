9e60482f2c0f520fec53b7eda78f654727b0fe90
kishi <trsasasusu@gmail.com>
2016-05-27 17:10:41 +0900

    cemx become same as ConEmu like s, sV, ...

--- open_pane.cpp
+++ open_pane.cpp
@@ -4,15 +4,7 @@
 #include "cemx.h"
 
 void OpenPane::OpenBash(std::string bash_path, std::string option) {
-	std::string str_option = "";
-	if(option == "%") {
-		str_option = "s";
-	}
-	if(option == "\"") {
-		str_option = "sV";
-	}
-
-	std::string str_system = "cmd /c \"" + bash_path + " --login -i -new_console:" + str_option + "\"";
+	std::string str_system = "cmd /c \"" + bash_path + " --login -i -new_console:" + option + "\"";
 
 	std::cout << str_system << std::endl;
 
